63ef66eaa10f535d8fa6b1ca0b2b59680fdef4d8
RandomPigYT <vinecompilation864@gmail.com>
2023-06-23 01:02:46 +0530

    formatting

--- c_extension/src/shaderIO.c
+++ c_extension/src/shaderIO.c
@@ -3,16 +3,13 @@
 #include "../include/extension.h"
 
 void sendToShader(struct SceneData scene, struct argData args) {
-
-    printf("cameraPos %f %f %f\ncameraDir %f %f %f\n", scene.cameraPos[0], scene.cameraPos[1],
-           scene.cameraPos[2], scene.cameraDir[0], scene.cameraDir[1],
-           scene.cameraDir[2]);
-    printf("program - %d\n", scene.shaderProgram);
+  printf("cameraPos %f %f %f\ncameraDir %f %f %f\n", scene.cameraPos[0],
+         scene.cameraPos[1], scene.cameraPos[2], scene.cameraDir[0],
+         scene.cameraDir[1], scene.cameraDir[2]);
+  printf("program - %d\n", scene.shaderProgram);
   for (int i = 0; i < args.numMeshes; i++) {
-
     printf("%ld %ld %ld %ld\n", scene.meshes[i].startingVertex,
            scene.meshes[i].numTriangles, scene.meshes[i].materialID,
            scene.meshes[i].objectID);
-
   }
 }
