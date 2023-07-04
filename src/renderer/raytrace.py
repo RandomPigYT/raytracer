122fda1fee7e45ba2b37fb8261616667e093f106
RandomPigYT <vinecompilation864@gmail.com>
2023-07-04 18:04:25 +0530

    save

--- src/renderer/raytrace.py
+++ src/renderer/raytrace.py
@@ -17,5 +17,6 @@ def raytrace(scene: sc.Scene, maxBounces, raysPerPixel):
     gl.glUniform1ui(maxBouncesLoc, maxBounces)
 
     gl.glDispatchCompute(scene.resolution[0], scene.resolution[1], 1)
-    gl.glMemoryBarrier(gl.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT)
+    #gl.glMemoryBarrier(gl.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT)
+    gl.glFinish()
 
