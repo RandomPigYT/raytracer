5830d8101c8f631741aa37d891a5741545e7448f
RandomPigYT <vinecompilation864@gmail.com>
2023-07-19 19:25:54 +0530

    Whit spots gone. Don't know why tho

--- src/renderer/render.py
+++ src/renderer/render.py
@@ -31,7 +31,7 @@ def render(window, scene, impl: GlfwRenderer):
         
         gl.glClear(gl.GL_COLOR_BUFFER_BIT)
 
-        rt.raytrace(scene, 10, 50)
+        rt.raytrace(scene, 10, 5)
         cam.move()
 
         shader.useShader(scene.shaderProgram)
