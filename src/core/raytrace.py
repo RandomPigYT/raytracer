df2147bb048f5366f5281a6df56811c034bb4ee2
RandomPigYT <vinecompilation864@gmail.com>
2023-08-28 11:54:10 +0530

    used scene that's passed into raytrace instead of currentsScene

--- src/core/raytrace.py
+++ src/core/raytrace.py
@@ -10,28 +10,28 @@ import graphics.shader as shader
 def raytrace(scene, maxBounces, raysPerPixel):
     global framNum
 
-    sm.currentScene.sceneRenderer.frameNum += 1
+    scene.sceneRenderer.frameNum += 1
 
-    raysPerPixelLoc = gl.glGetUniformLocation(sm.currentScene.sceneRenderer.compute, "raysPerPixel")
-    maxBouncesLoc = gl.glGetUniformLocation(sm.currentScene.sceneRenderer.compute, "maxBounces")
-    timeLoc = gl.glGetUniformLocation(sm.currentScene.sceneRenderer.compute, "time")
-    frameNumLoc = gl.glGetUniformLocation(sm.currentScene.sceneRenderer.compute, "frameNum")
+    raysPerPixelLoc = gl.glGetUniformLocation(scene.sceneRenderer.compute, "raysPerPixel")
+    maxBouncesLoc = gl.glGetUniformLocation(scene.sceneRenderer.compute, "maxBounces")
+    timeLoc = gl.glGetUniformLocation(scene.sceneRenderer.compute, "time")
+    frameNumLoc = gl.glGetUniformLocation(scene.sceneRenderer.compute, "frameNum")
 
-    gl.glUseProgram(sm.currentScene.sceneRenderer.compute)
+    gl.glUseProgram(scene.sceneRenderer.compute)
 
     gl.glUniform1ui(raysPerPixelLoc, raysPerPixel)
     gl.glUniform1ui(maxBouncesLoc, maxBounces)
 
     gl.glUniform1ui(timeLoc, int(time.time() * 10000))
-    gl.glUniform1ui(frameNumLoc, sm.currentScene.sceneRenderer.frameNum)
+    gl.glUniform1ui(frameNumLoc, scene.sceneRenderer.frameNum)
 
     gl.glDispatchCompute(48, 45, 1)
     gl.glMemoryBarrier(gl.GL_SHADER_IMAGE_ACCESS_BARRIER_BIT)
 
-    shader.useShader(sm.currentScene.sceneRenderer.shaderProgram)
+    shader.useShader(scene.sceneRenderer.shaderProgram)
 
     gl.glActiveTexture(gl.GL_TEXTURE0)
-    gl.glBindTexture(gl.GL_TEXTURE_2D, sm.currentScene.sceneRenderer.tex)
+    gl.glBindTexture(gl.GL_TEXTURE_2D, scene.sceneRenderer.tex)
 
     gl.glDrawElements(gl.GL_TRIANGLES, 6, gl.GL_UNSIGNED_INT, None)
     # gl.glFinish()
