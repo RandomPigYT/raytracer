dc2fbc00358bd829da48e656aa643f891275bf06
RandomPigYT <vinecompilation864@gmail.com>
2023-09-16 20:46:03 +0530

    Minor changes

--- src/main.py
+++ src/main.py
@@ -52,7 +52,7 @@ def main():
     scene.sceneRenderer.materials[scene.sceneRenderer.meshes[0].materialID].albedo  = (ct.c_float * 4)(*(249 / 255, 170 / 255, 70 / 255, 0))
 
     scene.loadModel("models/cube.obj")
-    #scene.loadModel("models/car.obj")
+    scene.loadModel("models/car.obj")
     # scene.loadModel("models/myCornellBox.obj")
     # scene.loadModel("models/CornellBox-Original.obj")
     scene.createSphere(0.73, (ct.c_float * 4)(0.96, 0.05, 2.37, 0))
